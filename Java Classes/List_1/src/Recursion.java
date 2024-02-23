f731b3400f60c1fbde64d34bfe6b211d909cd587
Dawid Śleziak <dawid.sleziak@gmail.com>
2024-02-23 21:18:58 +0100

    Second task finish

--- Java Classes/List_1/src/Recursion.java
+++ Java Classes/List_1/src/Recursion.java
@@ -7,9 +7,25 @@ class Recursion {
         System.out.println("Podaj liczbe n: ");
         int n = scanner.nextInt();
         System.out.println(factorial(n));
+        System.out.println(factorialDivision(n));
     }
 
-    public static long factorial(int number) {
+    private static double factorialDivision(int number) {
+        double result = 0;
+        boolean mark = true;
+        for (double i = 1.0; i <= number; i++) {
+            if (mark) {
+                result += 1 / (i + (double)number);
+            } else {
+                result -= 1 / (i + (double)number);
+            }
+
+            mark = !mark;
+        }
+        return result;
+    }
+
+    private static long factorial(int number) {
         int startNumber = 1;
         long result = 1L;
 
