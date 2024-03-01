4936329c4f6d77e21abd9c0d9c457c10a9f37a59
Dawid Śleziak <dawid.sleziak@gmail.com>
2024-03-01 22:23:00 +0100

    Overloadig view function

--- Java Classes/List_1/src/Exercise_Three/View.java
+++ Java Classes/List_1/src/Exercise_Three/View.java
@@ -1,6 +1,17 @@
 package Exercise_Three;
 
 public class View {
+
+    public String createAlbumDescription(DiskCD album) {
+        StringBuilder resultString = new StringBuilder();
+        resultString.append(album.getAlbumTitle()).append(" | ");
+        resultString.append(album.getAuthorSurname()).append(" | ");
+        resultString.append(album.getPublisherName()).append(" | ");
+        resultString.append(album.getReleaseDate()).append(" | ");
+        resultString.append(album.getPrice());
+
+        return resultString.toString();
+    }
     public String createAlbumDescription(int count, DiskCD album) {
         StringBuilder resultString = new StringBuilder();
         resultString.append(" ").append(count).append(" | ");
